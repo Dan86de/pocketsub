91d219275c496354bdf72d8bc770308dbb145461
Daniel Noworyta <daniel.noworyta@gmail.com>
2022-03-20 18:11:40 +0100

    Make dev script work.

--- app/root.tsx
+++ app/root.tsx
@@ -6,31 +6,31 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from 'remix'
-import type { MetaFunction } from 'remix'
-import styles from './tailwind.css'
+} from "remix";
+import type { MetaFunction } from "remix";
+import styles from "./tailwind.css";
 
 export const meta: MetaFunction = () => ({
-  charset: 'utf-8',
-  title: 'Pocketsub',
-  viewport: 'width=device-width,initial-scale=1',
-})
+  charset: "utf-8",
+  title: "Pocketsub",
+  viewport: "width=device-width,initial-scale=1",
+});
 
-export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }]
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full bg-gray-50">
       <head>
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="h-full">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
-  )
+  );
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,109 @@
-export default function Index() {
+import {
+  ActionFunction,
+  Form,
+  json,
+  LoaderFunction,
+  redirect,
+  useLoaderData,
+} from "remix";
+
+export const loader: LoaderFunction = async () => {
+  return json([
+    { id: "1", name: "Pants" },
+    { id: "2", name: "Jacket" },
+  ]);
+};
+
+export const action: ActionFunction = async ({ request }) => {
+  console.log(request.body);
+  return redirect("/app");
+};
+
+export const Index = () => {
+  const data = useLoaderData();
+  console.log(data);
   return (
-    <div className="text-xs">
-      <h1>Hello World</h1>
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <Form className="space-y-6" method="post">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email address
+              </label>
+              <div className="mt-1">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <div className="mt-1">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  Remember me
+                </label>
+              </div>
+
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-medium text-indigo-600 hover:text-indigo-500"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                Sign in
+              </button>
+            </div>
+          </Form>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Index;
