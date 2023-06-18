894282e7d76a097a5d997330420c25916eab3313
Daniel Noworyta <daniel.noworyta@gmail.com>
2023-06-18 17:59:05 +0200

    Header and logo.

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,3 +1,7 @@
+import Link from "next/link";
+import logo from "../../public/img/Logo.png";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,19 @@ export default function RootLayout({
 }) {
   return (
     <div>
-      <header>Marketing header</header>
+      <header className="mx-auto flex max-w-7xl items-center p-6">
+        <Link href={"/"} className="flex items-center gap-2">
+          <Image src={logo} alt="Pocketsub logo" height={34} />
+          <h1 className="text-xl font-bold">Pocketsub</h1>
+        </Link>
+        <nav className="w-full">
+          <ul className="flex items-center justify-center gap-4">
+            <li>
+              <Link href={"/blog"}>Blog</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
       {children}
     </div>
   );
